7e600fcd8ab461fbae32f19f581a97daadcb729b
carcent <carli.centurion@gmail.com>
2025-11-01 15:49:48 -0300

    added controllers, mongo connect and fixed little mistakes.

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,8 @@ router.get('/', (req, res) => {
   res.send('Welcome to the Home Page!');
 });
 
+router.use('/contacts', require('./contacts'));
+
+
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+require('dotenv').config();
+
+const mongodb = require('./data/database');
 const app = express();
 const port = process.env.PORT || 8080;
 
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+
+
+
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);   
+    } 
+    else {
+        app.listen(port, () => {console.log(`Database is listening and server is running on port ${port}`)});
+        }
 });
+
+
