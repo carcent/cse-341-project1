7b1299cd87ad90bea24b359b6ef8e2806504c90f
carcent <carli.centurion@gmail.com>
2025-11-05 22:15:05 -0300

    week 2. Swagger

--- server.js
+++ server.js
@@ -3,22 +3,21 @@ require('dotenv').config();
 
 const mongodb = require('./data/database');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 const port = process.env.PORT || 8080;
 
-app.use(express.json());
-
-app.use('/', require('./routes'));
-
-
-
+app
+  .use(express.json())
+  .use('/', require('./routes'))
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err) => {
   if (err) {
-    console.log(err);   
-    } 
-    else {
-        app.listen(port, () => {console.log(`Database is listening and server is running on port ${port}`)});
-        }
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database is listening and server is running on port ${port}`);
+    });
+  }
 });
-
-
