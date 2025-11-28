89eb23de6738f6905574fef7256d49344e25c5f1
carcent <carli.centurion@gmail.com>
2025-11-28 16:46:37 -0300

    new changes back to basic

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,6 +1,8 @@
 const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) return next();
-  return res.status(401).json({ message: 'Not authenticated' });
+  if (!req.session.user) {
+    return res.status(401).json('You do not have access.');
+  }
+  next();
 };
 
 module.exports = { isAuthenticated };
